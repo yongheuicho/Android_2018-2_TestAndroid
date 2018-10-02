0d520ceb52a77beea154bef16b2ff71e8c59caf0
yongheuicho <yongheuicho@gmail.com>
2018-10-02 16:30:15 +0900

    [2018-10-02] add the homepage

--- app/src/main/java/kr/ac/mokwon/ice/testandroid/MainActivity.java
+++ app/src/main/java/kr/ac/mokwon/ice/testandroid/MainActivity.java
@@ -1,24 +1,43 @@
 package kr.ac.mokwon.ice.testandroid;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    protected Button btHomepage;
+    protected Button btHomepage, btDial, btCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btHomepage = (Button)findViewById(R.id.btHomepage);
+        btHomepage = (Button) findViewById(R.id.btHomepage);
         btHomepage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://ice.mokwon.ac.kr"));
+                startActivity(intent);
+            }
+        });
+        btDial = (Button) findViewById(R.id.btDial);
+        btDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:0428297670"));
+                startActivity(intent);
+            }
+        });
+        btCall = (Button) findViewById(R.id.btCall);
+        btCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:0428297670"));
                 startActivity(intent);
             }
         });
