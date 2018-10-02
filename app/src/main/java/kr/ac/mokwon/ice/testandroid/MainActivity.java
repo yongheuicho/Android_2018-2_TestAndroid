e4204c048859fde710b97807d9efeceb9cec0781
yongheuicho <yongheuicho@gmail.com>
2018-10-02 16:12:01 +0900

    [2018-10-02] add the homepage

--- app/src/main/java/kr/ac/mokwon/ice/testandroid/MainActivity.java
+++ app/src/main/java/kr/ac/mokwon/ice/testandroid/MainActivity.java
@@ -1,13 +1,26 @@
 package kr.ac.mokwon.ice.testandroid;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    protected Button btHomepage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btHomepage = (Button)findViewById(R.id.btHomepage);
+        btHomepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://ice.mokwon.ac.kr"));
+                startActivity(intent);
+            }
+        });
     }
 }
--- mypush.bat
+++ mypush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[2018-10-02] init a project"
+git commit -m "[2018-10-02] add the homepage"
 git push -u origin master
